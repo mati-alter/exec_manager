9fc0f0ecb70f7f47b5cfcc788544834a18e7e074
mati-alter <mllanquimanmelipil@gmail.com>
2026-06-02 15:04:48 -0400

    chore: modify config and add dependecy

--- app/core/config.py
+++ app/core/config.py
@@ -2,9 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "Exec Manager"
-    debug: bool = False
-    database_url: str = ""
+    APP_NAME: str = "Exec Manager"
+    DEBUG: bool = False
+    DB_URL: str = ""
+    DB_PASS: str = ""
+    DB_PORT: int = 5432
+    REDIS_HOST: str = ""
+    REDIS_URL: str = ""
+    REDIS_PORT: int = 6379
+    REDIS_PASS: str = ""
 
     class Config:
         env_file = ".env"
